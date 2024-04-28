c12471dddcd9d50428bf760c24cf1d3cdf7628d4
kurtounet <bossut.anthony@gmail.com>
2024-04-28 22:05:29 +0200

    add ParticlesJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,9 @@ export class AppComponent {
 
   }
   ngOnInit(): void {
-    particlesJS.load("particles-js", "assets/particlesjs-config.json", null);
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', 'assets/particles.json', function () {
+      console.log('callback - particles.js config loaded');
+    });
   }
 }
--- src/app/dist/app.component.js
+++ src/app/dist/app.component.js
@@ -25,7 +25,10 @@ var AppComponent = /** @class */ (function () {
         console.log("start");
     }
     AppComponent.prototype.ngOnInit = function () {
-        particlesJS.load("particles-js", "assets/particlesjs-config.json", null);
+        /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+        particlesJS.load('particles-js', 'assets/particles.json', function () {
+            console.log('callback - particles.js config loaded');
+        });
     };
     AppComponent = __decorate([
         core_1.Component({
